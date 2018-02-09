6de5e3be9e87f94f04b233a74b5df0d51d23b88c
Wei Jun <im.weijun@gmail.com>
2018-02-09 13:04:55 +0000

    login/register

--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -1,12 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController} from 'ionic-angular';
 
-/**
- * Generated class for the RegisterPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
 @IonicPage()
 @Component({
